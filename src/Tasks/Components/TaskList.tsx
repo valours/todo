fb58e79697e2f5371f492df71904674448a9ded7
Valentin Le Flem <leflem.valentin+dev@gmail.com>
2020-07-13 19:39:30 +0200

    upgrade delete icon of task

--- src/Tasks/Components/TaskList.tsx
+++ src/Tasks/Components/TaskList.tsx
@@ -1,9 +1,10 @@
-import ListItem from "@material-ui/core/ListItem";
-import ListSubheader from "@material-ui/core/ListSubheader";
-import ListItemText from "@material-ui/core/ListItemText";
 import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
 import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import DeleteIcon from "@material-ui/icons/Delete";
 import React from "react";
 import { Task } from "../domain";
 
@@ -25,9 +26,11 @@ export default ({
       {tasks.map(t => (
         <ListItem key={t.id} onClick={() => deleteTaskById(t.id)}>
           <ListItemText primary={t.title} />
-          <IconButton edge="end" aria-label="comments">
-            <DeleteIcon />
-          </IconButton>
+          <ListItemSecondaryAction>
+            <IconButton edge="end" aria-label="delete">
+              <DeleteIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
         </ListItem>
       ))}
     </List>
